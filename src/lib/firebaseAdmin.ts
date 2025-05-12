d5ec6782bdbc4b38e4f1b99dc9dd19e9887bc288
Gabrielk127 <gabrielfsilva09@gmail.com>
2025-05-12 15:47:58 -0300

    add variables

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,19 +1,28 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "@/lib/serviceAccountKey.json";
 import type { ServiceAccount } from "firebase-admin";
 
-console.log("Conteúdo da conta de serviço:", serviceAccount); // Adicione este log
+const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
-if (!getApps().length) {
+if (!getApps().length && serviceAccountString) {
   try {
+    const serviceAccount = JSON.parse(serviceAccountString) as ServiceAccount;
     initializeApp({
-      credential: cert(serviceAccount as ServiceAccount),
+      credential: cert(serviceAccount),
     });
-    console.log("Firebase Admin SDK inicializado com sucesso.");
+    console.log(
+      "Firebase Admin SDK inicializado com sucesso usando variável de ambiente."
+    );
   } catch (error) {
-    console.error("Erro ao inicializar o Firebase Admin SDK:", error);
+    console.error(
+      "Erro ao inicializar o Firebase Admin SDK com variável de ambiente:",
+      error
+    );
   }
+} else if (!getApps().length) {
+  console.warn(
+    "Firebase Admin SDK não inicializado. Variável de ambiente FIREBASE_SERVICE_ACCOUNT_KEY não encontrada."
+  );
 }
 
 const db = getFirestore();
